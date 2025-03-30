c19a40f3781676ae81f7fedb08af74cfa5e3fc55
nichalada <nichaladasuphachayanon@gmail.com>
2025-03-31 00:34:23 +0700

    feat: create basic LandingPage component with header

---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+function LandingPage() {
+  return (
+    <div>
+      {/* Title */}
+      <h1 className="text-blue-400 font-bold text-5xl text-center p-8 mt-4">
+        เที่ยวไหนดี
+      </h1>
+      {/* Search Bar */}
+      {/* Tour List */}
+    </div>
+  );
+}
+
+export default LandingPage;
